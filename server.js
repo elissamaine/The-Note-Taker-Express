154db35416324d4530888d5d43eafbe1601fa109
elissamaine <emaine97@yahoo.com>
2023-04-09 17:21:57 -0700

    added html routs and added helper functions to read and write data from the forms

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-//helpers for reading and writing a file have to create a helpers folder and attach a uuid.js file (look at mini project)
-//const { readAndAppend, readFromFile } = require('.')
+//helpers for reading and writing a file these helper functions were from the mini-project
+const { readAndAppend, readFromFile, writeToFile  } = require('./helpers/helpers')
 // Helper method for generating unique ids (idk which one to use come back to this)
-const uuid = require('./helpers/uuid');
+
 const { v4: uuidv4 } = require('uuid');
 
 
@@ -12,7 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-//middleware
+//middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -20,18 +20,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //HTML ROUTES
+//get request for the home page
 app.get('/', (req, res) => {
-  
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+//get request for the notes page
 app.get('/notes', (req, res) => {
-
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 
 //API ROUTES 
+//get request for the json data in the db folder 
 app.get('/api/notes', (req, res) => {
-
+  res.sendFile
 });
 
 app.post('/api/notes', (req, res) => {
